bb4df844c065d5a1a772c5fc072ec3adfba9caa7
arwabalawi <arwa62885@gmail.com>
2021-12-07 09:13:48 +0300

    completed

---
+++ LootLogger  12 Silver Challenge/LootLogger/Controller/DateCreatedViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DateCreatedViewController.swift
+//  LootLogger
+//
+//  Created by Ressam Al-Thebailah on 20/04/1443 AH.
+//
+
+import UIKit
+
+class DatePickerViewController: UIViewController {
+  var datePicker: UIDatePicker!
+  
+  var item: Item!
+
+  override func loadView() {
+    let view = UIView()
+    view.backgroundColor = UIColor.white
+    self.view = view
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    self.title = "Date Created"
+
+    datePicker = UIDatePicker()
+    datePicker.datePickerMode = .date
+    datePicker.date = item.dateCreated
+    datePicker.translatesAutoresizingMaskIntoConstraints = false
+    self.view.addSubview(datePicker)
+
+    datePicker.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    datePicker.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+  }
+
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    item.dateCreated = datePicker.date
+  }
+}
